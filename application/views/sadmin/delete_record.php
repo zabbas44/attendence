daed6180ea67eabeab3876921bb8c4aed3d36a43
zain <zabbas44@gmail.com>
2014-09-26 20:50:04 +0500

    edit and delete views

---
+++ application/views/sadmin/delete_record.php
@@ -0,0 +1,43 @@
+<div class="cotainer marginlr30">
+    <form action="<?php echo base_url()?>sadmin/delet_record" method="post">
+<table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%" style="background: whitesmoke">
+    
+    <thead class="table-header">
+            <tr>
+                <td>Serial #</td>
+                <td>Name</td>
+                <td>Designation</td>
+                <td>Joining Date</td>
+                <td>Action</td>
+            </tr>
+    </thead>
+    <tbody>
+                
+
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                        <p><b>This record is deleted from database</b></p>
+
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="5" class="table-footer">
+                        <a href="<?php echo base_url()?>sadmin/crud" class="btn btn-primary">Back To Admin</a>
+                    </td>
+                </tr>
+               
+          
+    </tbody>        
+
+    </table>
+</form>
+<div class="row">
+    <div class="col-lg-12" >
+    
+    </div>
+</div>
+</div>
---
+++ application/views/sadmin/edit_record.php
@@ -0,0 +1,43 @@
+<div class="cotainer marginlr30">
+    <form action="<?php echo base_url()?>sadmin/delet_record" method="post">
+<table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%" style="background: whitesmoke">
+    
+    <thead class="table-header">
+            <tr>
+                
+                <td>Name</td>
+                <td>Designation</td>
+                <td>Joining Date</td>
+                
+            </tr>
+    </thead>
+    <tbody>
+                
+
+                <tr>
+                    <td><input type="text" class="form-control" /></td>
+                    <td><input type="text" class="form-control" /></td>
+                    <td><input type="text" class="form-control" /></td>
+                    
+                    
+                </tr>
+                <tr>
+                    <td colspan="5" class="table-footer">
+                        <a href="<?php echo base_url()?>sadmin/crud" class="btn btn-success">Submit Changes</a>
+                        &nbsp;
+                        <a href="<?php echo base_url()?>sadmin/crud" class="btn btn-primary">Back To Admin</a>
+                        
+                    </td>
+                </tr>
+               
+          
+    </tbody>        
+
+    </table>
+</form>
+<div class="row">
+    <div class="col-lg-12" >
+    
+    </div>
+</div>
+</div>
